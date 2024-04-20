e9fb971391b24f2c41efc3adfd761d76f64b427f
Prajwal Nadagouda <32608138+prajwalnadagouda@users.noreply.github.com>
2024-04-19 17:20:05 -0700

    update

--- app.py
+++ app.py
@@ -16,9 +16,24 @@ def getglobalmodelstatus():
 @app.route('/routes', methods=['POST'])
 def getroutes():
     try:
+        Days=['Monday','Tuesday','Wednesday','Thursday','Friday']
+        routes={}
         data = request.json
-        # selected_stock = data.get('stock')
-        return "Okay"
+        for day in Days:
+            dayRoute=[]
+
+            for i in range(1,23,10):
+                timeDetails={}
+                timeDetails['StartPoint']='abc'
+                timeDetails['EndPoint']='def'
+                timeDetails['Time']=str(i)+":23"
+                timeDetails['PeopleCount']=50
+                timeDetails['Stops']=['qwe','rty','uio']
+                dayRoute.append(timeDetails)
+
+            routes[day]=dayRoute
+        print(data)
+        return jsonify(routes)
     except:
         return jsonify({'error': 'Internal Server Error'}), 500
     
